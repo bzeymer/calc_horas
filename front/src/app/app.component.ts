64e03fc9d5e8aafdbfa7b843940d55f763cdccb1
Breno <bzeymer@gmail.com>
2018-03-12 10:05:33 -0300

    first communication between backend and frontend

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  http = axios.create({
+    baseURL: 'http://localhost:3000/',
+    timeout: 1000
+  });
+
+  ngOnInit() {
+    this.http.get('/').then((res) => {
+      console.log(res);
+      this.title = res.data;
+    });
+  }
 }
